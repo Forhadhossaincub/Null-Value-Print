0d517994ae3114625c2f61d4f2763892bfc232ea
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 14:41:22 +0600

    Add project files.

---
+++ Null Print/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? n = null;
+
+            Console.WriteLine(n.ToString());
+
+
+            if (n.HasValue)
+            {
+                Console.WriteLine(n.Value);
+            }
+
+            else
+            {
+                Console.WriteLine("Null");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
